d72ce6893a8706d3a91fd9d32f44ca998a236273
Thor <thornton.ellis@annkissam.com>
2018-06-06 10:28:07 -0400

    Resolve issue with overlapping favicon urls

--- app/models/url.rb
+++ app/models/url.rb
@@ -19,10 +19,11 @@ class Url < ApplicationRecord
 
 				return nil if web_response.nil?
 
-				favicon_path = self.get_favicon_string(web_response.to_s, base_url)
+				favicon_url = build_favicon_string(web_response.to_s, url_record.url, base_url)
+
 				# Update record
 				ActiveRecord::Base.transaction do
-					url_record.update_attributes(fav_url: url_record.url + favicon_path)
+					url_record.update_attributes(fav_url: favicon_url)
 				end
 			end
 
@@ -35,11 +36,11 @@ class Url < ApplicationRecord
 			web_response = self.is_url(http_url)
 
 			unless web_response.nil?
-				favicon_path = self.get_favicon_string(web_response.to_s, base_url)
+				favicon_url = build_favicon_string(web_response.to_s, http_url, base_url)
 
 				# Add to DB if url exists and return
 				ActiveRecord::Base.transaction do
-					return Url.create(url: http_url, fav_url: http_url + favicon_path).fav_url
+					return Url.create(url: http_url, fav_url: favicon_url).fav_url
 				end
 			end
 
@@ -69,13 +70,13 @@ class Url < ApplicationRecord
 	# HTTParty will return a response in which I can search for a substring like:
 	# <link rel=\"icon\" href=\"http://example.com/favicon.ico\" sizes=\"32x32\"/>
 	# I'll need to break down the string to get the favicon route and return
-	def self.get_favicon_string(response_string, url)
+	def self.get_favicon_path(response_string)
 		begin
 			# Check if the response contains the favicon info
 			if response_string.include?("<link rel=\"icon\"")
-				favicon_string = response_string.split("<link rel=\"icon\"").last
+				favicon_path = response_string.split("<link rel=\"icon\"").last
 			elsif response_string.include?("<link rel=\"shortcut icon\"")
-				favicon_string = response_string.split("<link rel=\"shortcut icon\"").last
+				favicon_path = response_string.split("<link rel=\"shortcut icon\"").last
 			else
 				# No favicon found in response so we'll try at the root
 				return '/favicon.ico'
@@ -85,20 +86,31 @@ class Url < ApplicationRecord
 			return '/favicon.ico'
 		end
 		# Breaking down the string until it's just the favicon link
-		favicon_string = favicon_string
+		favicon_path = favicon_path
 											.split('>').first
 											.split('href=').last
 											.gsub("'", '"') # (Some sites put their links in single quotes so let's make it consistant)
 											.split('"').second
-
-		# Some responses just contain a path from the host,
-		# so we'll treat them all the same to make things easier
-		self.strip_url(favicon_string).gsub(url, '')
 	end
 
 	def self.strip_url(url)
 		url.gsub('https://', '')
 			 .gsub('http://', '')
 			 .gsub('www.', '')
+	end
+
+	def self.build_favicon_string(response, http_url, base_url)
+		favicon_path = self.get_favicon_path(response)
+		# If the string starts with a forward slash, we add it to the host path
+		if (self.strip_url(favicon_path)[0] == '/')
+			favicon_url = http_url + favicon_path
+		else
+			favicon_url = favicon_path
+		end
+
+		# Check if favicon exists
+		favicon_response = self.is_url(favicon_url)
+		favicon_url = nil if favicon_response.nil?
+		return favicon_url
 	end
 end
